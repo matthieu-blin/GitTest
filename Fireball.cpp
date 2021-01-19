c720185a015292bbb87bbca467c05e0b7c9668b3
matthieu-blin <matth.blin@gmail.com>
2021-01-19 15:00:07 +0100

    Ajout de la fonction de rendu

--- Fireball.cpp
+++ Fireball.cpp
@@ -2,7 +2,16 @@
 class Fireball
 {
 	public :
-		void Render(float _dt);
+		void Render(float _dt)
+		{
+			//render of fireball
+			if(m_fireTex )
+			{
+				Render(m_fireTex);
+			}
+
+		}
+
 	private:
 		float m_damage = 0.f;
 
